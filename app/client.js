1b5c64262233dbd051cd5bed59f7c0c893c6d803
Yield-chan <yieldchan@gmail.com>
2021-12-29 19:57:53 -0600

    basic account updating

--- app/client.js
+++ app/client.js
@@ -1,11 +1,27 @@
 const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
-anchor.setProvider(anchor.Provider.local())
+const provider = anchor.Provider.local()
+
+anchor.setProvider(provider)
 
 async function main() {
     const program = anchor.workspace.AnchorTutorial
 
-    await program.rpc.initialize()
+    const myAccount = anchor.web3.Keypair.generate()
+
+    await program.rpc.initialize(new anchor.BN(1234), {
+        accounts: {
+            myAccount: myAccount.publicKey,
+            user: provider.wallet.publicKey,
+            systemProgram: SystemProgram.programId
+        },
+        signers: [myAccount],
+    })
+
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+
+    console.log(account)
 }
 
 console.log("Running client.")
--- tests/anchor-tutorial.ts
+++ tests/anchor-tutorial.ts
@@ -1,17 +1,47 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
+import { expect } from 'chai'
+
 import { AnchorTutorial } from '../target/types/anchor_tutorial';
+
+const { SystemProgram } = anchor.web3
 
 describe('anchor-tutorial', () => {
 
+  const provider = anchor.Provider.env()
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.AnchorTutorial as Program<AnchorTutorial>;
+  const myAccount = anchor.web3.Keypair.generate()
 
   it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
+
+    const tx = await program.rpc.initialize(new anchor.BN(1234), {
+      accounts: {
+        myAccount: myAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      },
+      signers: [myAccount]
+    })
+
     console.log("Your transaction signature", tx);
+
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+    expect(account.data.eq(new anchor.BN(1234)))
   });
+
+  it('account updated', async () => {
+    const tx = await program.rpc.update(new anchor.BN(6789), {
+      accounts: {
+        myAccount: myAccount.publicKey,
+      }
+    })
+
+    console.log(`Transaction sig ${tx}`)
+
+    const account = await program.account.myAccount.fetch(myAccount.publicKey)
+    expect(account.data.eq(new anchor.BN(6789)))
+  })
 });
